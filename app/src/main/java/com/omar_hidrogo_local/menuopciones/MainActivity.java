ee22caf6c12846c99e400654e7429cb2a047643b
Omar Hidrogo <omar.083090@gmail.com>
2017-07-10 16:05:22 -0500

    menu de contexto terminado

--- app/src/main/java/com/omar_hidrogo_local/menuopciones/MainActivity.java
+++ app/src/main/java/com/omar_hidrogo_local/menuopciones/MainActivity.java
@@ -3,8 +3,13 @@ package com.omar_hidrogo_local.menuopciones;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvNombre = (TextView) findViewById(R.id.tvNombre);
+
+        //se agrego el menu de contexto al texview
+        registerForContextMenu(tvNombre);
     }
 
     //para anclar el boton de menu y despliege el contenido
@@ -38,5 +48,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //crea un menu de contexto
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.menu_contexto, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mEdit:
+                //Intent o descargar segun lo que se realice
+                Toast.makeText(this, getResources().getString(R.string.menu_edit), Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.mDelete:
+                Toast.makeText(this, getResources().getString(R.string.menu_delete), Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
